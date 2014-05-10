cf9825b40b42932dbd583a9129e6226f3c005ff5
Sartorio <pablo.sartorio@carvajal.com>
2014-05-10 14:40:24 -0300

    MI COLEGIO
    VERSION 1

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// JavaScript Document
+var sectionManager = (function($){
+	
+	function materialEstudio( value ) {
+		
+	}
+	
+	function textosLiterarios( value ) {
+		
+	}
+	
+	function guiasDocentes( value ) {
+		
+	}
+	
+	function recursosDigitales( value ) {
+		
+	}
+	
+	function resetear( value ) {
+		
+	}
+	
+	return {
+		
+		materialEstudio 	: materialEstudio,
+		textosLiterarios 	: textosLiterarios,
+		guiasDocentes 		: guiasDocentes,
+		recursosDigitales 	: recursosDigitales,
+		resetear 			: resetear
+		
+	}
+})(jQuery);
